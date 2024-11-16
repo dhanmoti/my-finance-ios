36f94bce9afb5bdd2d95a2045dccbdc1d0617805
Devi <dhanmoti@gmail.com>
2024-11-16 10:40:40 +0800

    Create Xcode project

---
+++ myFinance/myFinance/myFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  myFinanceApp.swift
+//  myFinance
+//
+//  Created by Dhan Moti on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct myFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
