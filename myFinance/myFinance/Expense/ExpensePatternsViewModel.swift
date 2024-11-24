9543d98fef11125c07ec89043f7fd9c8125513f9
Devi <dhanmoti@gmail.com>
2024-11-24 16:18:33 +0800

    add project folder structure

---
+++ myFinance/myFinance/Expense/ExpensePatternsViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ExpensePatternsViewModel.swift
+//  myFinance
+//
+//  Created by Dhan Moti on 24/11/24.
+//
+
+import Foundation
+class ExpensePatternsViewModel {
+    
+    //private var listOfTransaction[Transaction]
+    
+    func analysePatterns(){
+        
+    }
+}
